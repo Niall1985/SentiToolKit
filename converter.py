fd88f85e3f0c5abc6bc51f0f741c7c5d4178d14f
Niall1985 <dcunhaniall@gmail.com>
2024-10-02 01:52:45 +0530

    added the dataset for neutral comment/review identification and training

--- converter.py
+++ converter.py
@@ -1,10 +1,10 @@
 import json
-with open('neg_dup.txt', 'r') as file:
+with open('neu_dup.txt', 'r') as file:
     lines = file.readlines()
 formatted_reviews = []
 for line in lines:
     review = line.strip()
-    formatted_reviews.append({"review": review, "sentiment": "negative"})
+    formatted_reviews.append({"review": review, "sentiment": "neutral"})
 json_data = json.dumps(formatted_reviews, indent=4)
-with open('negative_reviews.json', 'w') as json_file:
+with open('neutral_reviews.json', 'w') as json_file:
     json_file.write(json_data)
--- tensor_model.py
+++ tensor_model.py
@@ -8,6 +8,7 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout, Input
+from tensorflow.keras.utils import to_categorical
 
 
 load_dotenv()
@@ -19,7 +20,7 @@ df = pd.DataFrame(data)
 # print(df.head())
 
 reviews = df['review'].values
-sentiments = df['sentiment'].apply(lambda x:1 if x=='positive' else 0).values
+sentiments = df['sentiment'].apply(lambda x:2 if x=='positive' else(1 if x=='neutral' else 0)).values
 
 tokenizer = Tokenizer(num_words = 5000)
 tokenizer.fit_on_texts(reviews)
@@ -27,20 +28,20 @@ sequences = tokenizer.texts_to_sequences(reviews)
 
 maxlen = 100
 x = pad_sequences(sequences, maxlen=maxlen)
-
-x_train, x_test, y_train, y_test = train_test_split(x, sentiments, test_size=0.2, random_state=42)
+y_one_hot = to_categorical(sentiments, num_classes=3)
+x_train, x_test, y_train, y_test = train_test_split(x, y_one_hot, test_size=0.2, random_state=42)
 
 model = Sequential()
 model.add(Input(shape=(maxlen,))) 
 model.add(Embedding(input_dim=5000, output_dim=128, input_length=maxlen))
 model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2))
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(3, activation='softmax'))
 
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 model.summary()
 
-history = model.fit(x_train, y_train, epochs=5, batch_size=64,validation_split=0.2)
+history = model.fit(x_train, y_train, epochs=10, batch_size=64,validation_split=0.2)
 
 loss, accuracy = model.evaluate(x_test, y_test)
 print(f'Test Accuracy: {accuracy*100:.2f}%')
@@ -49,13 +50,14 @@ def predict_sentiment(sentence):
     sequence = tokenizer.texts_to_sequences([sentence])
     padded = pad_sequences(sequence, maxlen=maxlen)
     prediction = model.predict(padded)
-    if prediction>=0.5:
+    predicted_class = prediction.argmax(axis=-1)  
+    if predicted_class == 2:
         return 'Positive'
-    elif prediction>=0.299 and prediction<=4.99:
+    elif predicted_class == 1:
         return 'Neutral'
     else:
         return 'Negative'
-new_sentence = "I dislike this product, it is not at all good"
+new_sentence = "The battery life is poor and bad, but the display is superb and the keys are smooth"
 print(predict_sentiment(new_sentence))
 
 
