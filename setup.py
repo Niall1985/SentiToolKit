49da81dc799542fb24ffd89530ad4a78f9f98177
Niall1985 <dcunhaniall@gmail.com>
2024-10-30 16:50:09 +0530

    updated readme and setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="SentiToolKit",
-    version="0.1.1",
+    version="0.1.0",
     packages=find_packages(),
     install_requires=[
         "tensorflow>=2.0",
