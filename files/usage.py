ff56e8bd9fc43fdad0ba908f7aa70ee16271f941
Niall1985 <dcunhaniall@gmail.com>
2024-10-31 00:22:17 +0530

    setup.py and Main_tensor_model.py has been fixed to resolve import errors

--- files/usage.py
+++ files/usage.py
@@ -1,6 +1,8 @@
-from sentitoolkit.Main_tensor_model import SentiToolKit
+from SentiAnalyzer.Main_tensor_model import SentiToolKit
 
 model = SentiToolKit()
 rev = input("Enter: ")
 
 print(model.__call__(rev))
+# use_senti_toolkit.py
+
--- sentitoolkit/__init__.py
+++
@@ -1,2 +0,0 @@
-from SentiToolKit.Main_tensor_model import SentiToolKit
-__all__ = ['SentiToolKit']
