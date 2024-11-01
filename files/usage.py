5c82ec8254dc5b009c54f7d865ffbe2053b10ddd
Niall1985 <dcunhaniall@gmail.com>
2024-11-01 17:52:09 +0530

    checked the working of the lib

--- files/usage.py
+++ files/usage.py
@@ -1,8 +1,8 @@
-from SentiAnalyzer.Main_tensor_model import SentiToolKit
+# from SentiAnalyzer.Main_tensor_model import SentiToolKit
 
-model = SentiToolKit()
-rev = input("Enter: ")
+# model = SentiToolKit()
+# rev = input("Enter: ")
 
-print(model.__call__(rev))
-# use_senti_toolkit.py
+# print(model.__call__(rev))
+# # use_senti_toolkit.py
 
